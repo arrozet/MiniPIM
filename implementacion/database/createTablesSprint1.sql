adb2216f40ef4b9da771901bdbfe4e53ca491b1a
RubénO. <rubenoliva@uma.es>
2024-11-21 14:44:21 +0100

    Conectada db + creado proyecto + creadas tablas db

---
+++ implementacion/database/createTablesSprint1.sql
@@ -0,0 +1,43 @@
+-- Crear la tabla Categoria
+CREATE TABLE Categoria (
+    id VARCHAR(50) PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL
+);
+
+-- Crear la tabla Producto
+CREATE TABLE Producto (
+    sku VARCHAR(50) PRIMARY KEY,
+    gtin VARCHAR(50),
+    label VARCHAR(100),
+    fechaCreacion DATE NOT NULL,
+    ultimaModificacion DATE,
+    descripcionCorta VARCHAR(255),
+    descripcionLarga TEXT,
+    espacioOcupado INT,
+    thumbnail BLOB -- Para almacenar imágenes en formato jpg 200x200 píxeles
+);
+
+-- Crear la tabla AtributoPersonalizado
+CREATE TABLE AtributoPersonalizado (
+    id VARCHAR(50) PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    espacioOcupado INT
+);
+
+-- Crear tabla intermedia ProductoAtributo para la relación Producto-AtributoPersonalizado (0..5 - 0..*)
+CREATE TABLE ProductoAtributo (
+    producto_sku VARCHAR(50),
+    atributo_id VARCHAR(50),
+    PRIMARY KEY (producto_sku, atributo_id),
+    FOREIGN KEY (producto_sku) REFERENCES Producto(sku) ON DELETE CASCADE,
+    FOREIGN KEY (atributo_id) REFERENCES AtributoPersonalizado(id) ON DELETE CASCADE
+);
+
+-- Crear tabla intermedia ProductoCategoria para la relación Producto-Categoria (0..* - 0..*)
+CREATE TABLE ProductoCategoria (
+    producto_sku VARCHAR(50),
+    categoria_id VARCHAR(50),
+    PRIMARY KEY (producto_sku, categoria_id),
+    FOREIGN KEY (producto_sku) REFERENCES Producto(sku) ON DELETE CASCADE,
+    FOREIGN KEY (categoria_id) REFERENCES Categoria(id) ON DELETE CASCADE
+);
---
+++ implementacion/database/deleteAllTables.sql
@@ -0,0 +1,8 @@
+-- Borrar nuevas tablas (hay borrado en cascasda)
+/* 
+DROP TABLE IF EXISTS ProductoCategoria;
+DROP TABLE IF EXISTS ProductoAtributo;
+DROP TABLE IF EXISTS Producto;
+DROP TABLE IF EXISTS AtributoPersonalizado;
+DROP TABLE IF EXISTS Categoria;
+*/
