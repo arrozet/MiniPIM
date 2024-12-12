429ec33c7116cf4ef3bc28cda52cd3d58eec1098
RubénO. <rubenoliva@uma.es>
2024-12-12 18:13:35 +0100

    alomeon aora no faya

--- implementacion/MiniPIM/MiniPIM/Program.cs
+++ implementacion/MiniPIM/MiniPIM/Program.cs
@@ -1,23 +1,37 @@
 ﻿using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace MiniPIM
 {
     internal class Program
     {
+        private static Mutex mutex = null;
+
         [STAThread]
         static void Main()
         {
+            const string appName = "MiniPIMApp"; // Nombre único para tu aplicación
+            bool createdNew;
+
+            // Crear el mutex
+            mutex = new Mutex(true, appName, out createdNew);
+
+            if (!createdNew)
+            {
+                // Ya hay una instancia corriendo
+                MessageBox.Show("Another instance of MiniPIM is already running.", "Instance Running", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return; // Salir del programa
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            // Iniciar el formulario de productos
-
-            //Application.Run(new TestForm());
-
+            // Iniciar el formulario principal
             Application.Run(new Product.ProductosResumen());
-            Application.Exit();
 
+            // Liberar el mutex al cerrar
+            GC.KeepAlive(mutex);
         }
     }
 }
